9366cdcc20a8316fcdb0016d3b37ffaa76f27b5b
Roman <professor360025@gmail.com>
2021-10-02 16:43:58 +0500

    Добавлена обработка stationlog

--- mapper.py
+++ mapper.py
@@ -1,7 +1,6 @@
 from hdfs import InsecureClient
 import pandas as pd
 import sys
-import csv
 
 client_hdfs = InsecureClient('http://localhost:9870')
 
@@ -20,49 +19,35 @@ def process_files(log_file):
 
     return df
 
-
-# def create_h_files(log_files):
-#     file_name_read = '/new_files_3/'
-#     first_file = log_files.pop()
-#     name_of_h = first_file.split('.')[1]
-#     with client_hdfs.read(file_name_read + first_file, encoding='utf-8') as File:
-#         general_df = pd.read_csv(File)
-#
-#
-#     # for i in log_files:
-#     #     file_name_read = '/new_files_3/' + i
-#     #     with client_hdfs.read(file_name_read, encoding='utf-8') as File1:
-#     #         df = pd.read_csv(File1)
-#     #         general_df = pd.concat([df, general_df], axis=0)
-#
-#     general_df = general_df.groupby(['Day', '\tTickTime']).sum()
-#     new_name_of_file = '/practice_3_h_files/' + name_of_h + '.csv'
-#     with client_hdfs.write(new_name_of_file, encoding='utf-8') as NewFile:
-#         general_df.to_csv(NewFile)
-
-def create_h_file(log_file):
-    file_name_read = '/list_h_3/'
-    with client_hdfs.read(file_name_read + log_file, encoding='utf-8') as File:
-        general_df = pd.read_csv(File)
-
-
-    general_df = general_df.groupby(['Day', '\tTickTime']).sum()
-    new_name_of_file = '/practice_3_h_files/' + 'h7' + '.csv'
-    with client_hdfs.write(new_name_of_file, encoding='utf-8') as NewFile:
-        general_df.to_csv(NewFile)
+def process_station_log(log_file):
+    if 'stationlog' in log_file:
+        file_name_read = '/stationlog/' + log_file
+        file_name_write = '/new_stationlog/' + log_file
+    else:
+        file_name_read = '/list_h_3/' + log_file
+        file_name_write = '/new_files_3/' + log_file
+    with client_hdfs.read(file_name_read, encoding='utf-8') as File:
+        with client_hdfs.write(file_name_write, encoding='utf-8') as NewFile:
+            df = pd.read_csv(File)
+            if 'list_h_3' in file_name_read:
+                df.drop(df.index[(df['Day'] != 7)], axis=0, inplace=True)
+            df['\tTickTime'] = df['\tTickTime'] / 300
+            df['\tTickTime'] = df['\tTickTime'].astype(int)
+            if 'list_h_3' in file_name_read:
+                df = df.groupby(['Day', '\tTickTime'])['\tSpeed'].sum()
+            else:
+                df = df.groupby(['Day', '\tTickTime'])['\tError'].sum()
+            df.to_csv(NewFile)
 
 
 
 
 
-list_of_files = []
-general_df = None
-file_name_read = '/new_files_3/'
+
+
 
 for i in sys.stdin:
     i = i.rstrip()
-    process_files(i)
-    list_of_files.append(i)
+    process_station_log(i)
     print(i)
 
-# create_h_files(list_of_files)
--- reducer.py
+++ reducer.py
@@ -5,10 +5,16 @@ from hdfs import InsecureClient
 client_hdfs = InsecureClient('http://localhost:9870')
 
 general_df = None
+name_of_h = None
 
 for i in sys.stdin:
     i = i.rstrip()
-    file_name_read = '/new_files_3/' + i
+    if name_of_h is None:
+        name_of_h = i.split('.')[1]
+    if 'userlog' in i:
+        file_name_read = '/new_files_3/' + i
+    else:
+        continue
     with client_hdfs.read(file_name_read, encoding='utf-8') as File:
         df = pd.read_csv(File)
         if general_df is not None:
@@ -18,6 +24,6 @@ for i in sys.stdin:
 
 general_df = general_df.groupby(['Day', '\tTickTime']).sum()
 
-new_name_of_file = '/practice_3_h_files/' + 'h7' + '.csv'
+new_name_of_file = '/practice_3_h_files/' + name_of_h + '.csv'
 with client_hdfs.write(new_name_of_file, encoding='utf-8') as NewFile:
     general_df.to_csv(NewFile)
